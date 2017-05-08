e1023cd645ff33902c783bd63b694783835dd848
Michael Oliveira <michaeloc9@gmail.com>
2017-05-08 13:28:34 -0300

    Updating scss code of footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { ComparisonsModule } from './comparisons/comparisons.module';
 import { SharedModule } from './shared/shared.module';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { FooterComponent } from "app/footer/footer.component";
+import { FooterComponent } from 'app/footer/footer.component';
 
 @NgModule({
   declarations: [
