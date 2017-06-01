b01bfad76ae22f16d400bdcd18bb86a1ec9d1bb0
Michael Oliveira <michaeloc9@gmail.com>
2017-06-01 17:52:58 -0300

    Made some modifications on the structure of footer.component.html and .scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent { }
+export class AppComponent {
+  socialItems = [
+    {url: 'https://www.facebook.com', name: 'Facebook'},
+    {url: 'https://www.instagram.com', name: 'Instagram'},
+    {url: 'https://www.twitter.com', name: 'Twitter'}
+  ];
+  pageItems = [
+    {url: 'https://www.python.org/', name: 'Python'},
+    {url: 'https://pypi.python.org/pypi', name: 'PyPI'},
+    {url: 'https://www.pycon.org/', name: 'PyCon'}
+  ];
+}
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: 'footer.component.html',
   styleUrls: ['footer.component.scss'],
 })
-export class FooterComponent { }
+export class FooterComponent {
+    title = 'Pypicompare';
+    content = 'Lorem Ipsum is simply dummy' +
+    'text of the printing and typesetting' +
+    'industry. Lorem Ipsum has been the' +
+    'industry\'s standard dummy text ever' +
+    'since the 1500s, when an unknown printer' +
+    'into electronic typesetting, remaining' +
+    'essentially unchanged. It was popularised' +
+    'in the 1960s with the release of Letraset' +
+    'sheets containing Lorem Ipsum passages,' +
+    'and more recently with desktop publishing' +
+    'of Lorem Ipsum.';
+    footer = 'Copyright © 2017 - All Rights Reserved';
+ }
