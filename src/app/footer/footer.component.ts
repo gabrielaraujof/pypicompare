0662fa7478113e933398402da6e04f54d31db0c4
Michael Oliveira <michaeloc9@gmail.com>
2017-06-27 15:09:16 -0300

    Formatted files to 2 spaces.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,17 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['footer.component.scss'],
 })
 export class FooterComponent {
-    title = 'Pypicompare';
-    content = 'Lorem Ipsum is simply dummy' +
-    'text of the printing and typesetting' +
-    'industry. Lorem Ipsum has been the' +
-    'industry\'s standard dummy text ever' +
-    'since the 1500s, when an unknown printer' +
-    'into electronic typesetting, remaining' +
-    'essentially unchanged. It was popularised' +
-    'in the 1960s with the release of Letraset' +
-    'sheets containing Lorem Ipsum passages,' +
-    'and more recently with desktop publishing' +
-    'of Lorem Ipsum.';
-    footer = 'Copyright © 2017 - All Rights Reserved';
- }
+  title = 'Pypicompare';
+  content = 'Lorem Ipsum is simply dummy' +
+  'text of the printing and typesetting' +
+  'industry. Lorem Ipsum has been the' +
+  'industry\'s standard dummy text ever' +
+  'since the 1500s, when an unknown printer' +
+  'into electronic typesetting, remaining' +
+  'essentially unchanged. It was popularised' +
+  'in the 1960s with the release of Letraset' +
+  'sheets containing Lorem Ipsum passages,' +
+  'and more recently with desktop publishing' +
+  'of Lorem Ipsum.';
+  footer = 'Copyright © 2017 - All Rights Reserved';
+}
